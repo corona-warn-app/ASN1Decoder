6799658a8dde751d932896282ee9cda912a8f384
Artur Friesen <mail@arturfriesen.de>
2021-11-11 16:13:14 +0100

    Add OID for rsassa-pss 1.2.840.113549.1.1.10

--- ASN1Decoder/OID.swift
+++ ASN1Decoder/OID.swift
@@ -18,6 +18,7 @@ public enum OID: String {
     case ecdsaWithSHA256 = "1.2.840.10045.4.3.2"
     case ecdsaWithSHA512 = "1.2.840.10045.4.3.4"
     case rsaEncryption = "1.2.840.113549.1.1.1"
+    case rsaSsaPss = "1.2.840.113549.1.1.10"
     case sha256WithRSAEncryption = "1.2.840.113549.1.1.11"
     case md5WithRSAEncryption = "1.2.840.113549.1.1.4"
     case sha1WithRSAEncryption = "1.2.840.113549.1.1.5"
